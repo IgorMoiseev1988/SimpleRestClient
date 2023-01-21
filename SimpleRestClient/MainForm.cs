73856f8cb730a4d66afe4a5ee16fdc041f546594
Oleg Moiseev <wizardhelg@gmail.com>
2023-01-22 02:33:58 +0300

    Тестовый запрос к API

--- SimpleRestClient/MainForm.cs
+++ SimpleRestClient/MainForm.cs
@@ -1,21 +1,71 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
 namespace SimpleRestClient
 {
     public partial class MainForm : Form
     {
+        private readonly HttpClient client;
+
         public MainForm()
         {
             InitializeComponent();
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://jsonplaceholder.typicode.com")
+            };
+            //client = new HttpClient();
         }
 
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form about = new AboutForm();
             about.ShowDialog();
         }
 
-        private void ButtonExecute_Click(object sender, EventArgs e)
+        private async void ButtonExecute_Click(object sender, EventArgs e)
         {
-            
+            var request = new HttpRequestMessage(HttpMethod.Get, textBoxRequest.Text);
+
+            SetHeaders(request);
+
+            var response = await client.SendAsync(request);
+
+            //Ёто использу€ срань от мелком€гких
+            //if (response?.IsSuccessStatusCode ?? false)
+            //{
+            //    var str = await response.Content.ReadAsStringAsync();
+            //    var json = JsonDocument.Parse(str);
+
+            //    using var stream = new System.IO.MemoryStream();
+            //    using var writer =new Utf8JsonWriter(stream, new JsonWriterOptions() { Indented = true});
+            //    json.WriteTo(writer);
+            //    writer.Flush();
+
+            //    textBoxResponse.Text = System.Text.Encoding.UTF8.GetString(stream.ToArray());
+            //}
+
+            //ј это через Newtonsoft.json.  стати в втб он юзаетс€. Ћицензи€ у него MIT
+            if(JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()) is object obj)
+                textBoxResponse.Text = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+        }
+
+        private void SetHeaders(HttpRequestMessage message)
+        {
+
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+
+
         }
     }
 }
